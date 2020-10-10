8ee72874dd869f079477aabf73d88e01544214d1
Siddhesh Dalvi <dalvisiddhesh09@gmail.com>
2020-10-10 15:15:05 +0530

    Refactor shell script
    Removed if statements, added functions, code cleanup.

--- fedorapostinstall.sh
+++ fedorapostinstall.sh
@@ -2,52 +2,57 @@
 #
 # Fedora Post Install
 #
+# Description: A shell script for enabling RPMFusion repositories, installing multimedia codecs and enabling flatpak on Fedora.
+# 
 # The following terminal commands are taken from https://rpmfusion.org/Configuration/ and https://flatpak.org/setup/Fedora/
-#
-# Description: A shell script for enabling RPMFusion repositories, installing multimedia codecs and enabling flatpak on Fedora
 
-echo Fedora Post Install
-echo Script Started
+echo Fedora Post Install && echo Script Started.
 
-# To enable access to both the free and the nonfree repository use the following command: 
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+enable_rpmfusion_repo()
+{
+	# To enable access to both the free and the nonfree repository use the following command: 
+	sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+	https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+}
 
-if [ 0 == $? ]
-then
-    # To install appstream-data:
-    sudo dnf groupupdate core -y
+install_mulmedia_tainted_repo()
+{
+	# To install appstream-data:
+	sudo dnf groupupdate core -y
 
-    # To install the complements multimedia packages needed by gstreamer enabled applications:
-    sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+	# To install the complements multimedia packages needed by gstreamer enabled applications:
+	sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
 
-    # To install the sound-and-video complement packages needed by some applications:
-    sudo dnf groupupdate sound-and-video -y
+	# To install the sound-and-video complement packages needed by some applications:
+	sudo dnf groupupdate sound-and-video -y
 
-    # To enable tainted repositories
-    sudo dnf install rpmfusion-free-release-tainted -y && \
-    sudo dnf install rpmfusion-nonfree-release-tainted -y
-else
-    echo ERROR... Run Script Again
-    exit 0
-fi
+	# To enable tainted repositories
+	sudo dnf install rpmfusion-free-release-tainted -y
+	sudo dnf install rpmfusion-nonfree-release-tainted -y
+}
 
-# To enable flatpak repository
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo;
+enable_flatpak_repo()
+{
+	# To enable flatpak repository
+	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+}
 
-if [ 0 != $? ]
-then 
-	echo ERROR... Run Script Again
-	exit 0
-fi
+error()
+{
+	# Error message
+	echo ERROR! Run Script Again. && exit 0
+}
 
-echo Script Completed
-echo Success!
+success()
+{
+	#Success message
+	echo Script Completed. && echo Success! && sleep 1
+}
 
-sleep 1
+cleanup()
+{	
+	# Delete script, close terminal and exit
+	rm -r fedorapostinstall.sh ; kill -9 $PPID ; exit 0
+}
 
-rm -r fedorapostinstall.sh
-
-kill -9 $PPID
-
-exit 0
+enable_rpmfusion_repo && install_mulmedia_tainted_repo && enable_flatpak_repo && success && cleanup || error
